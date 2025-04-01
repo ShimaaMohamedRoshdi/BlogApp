be7eb31950b572f82ea220d14b83b46687942e41
shimaa mohamed <161171233+ShimaaMohamedRoshdi@users.noreply.github.com>
2025-04-01 22:42:50 +0200

    solve some prob

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  // othor next config here...
+ 
+  swcMinify: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production' ? true : false,
   },
